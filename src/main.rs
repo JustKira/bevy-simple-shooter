8c2832bb53db6f22b29ddced841afec93895afa8
zetaBUE <zeyad236389@bue.edu.eg>
2024-08-19 00:52:34 +0300

    chore Acceleration and Deceleration Controls (W and S) #4
    adds forward acceleration by adding 0.1s
    
    decelerates by calculating total velocity then creating a scale to calculate each axis alone

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{input::keyboard::KeyboardInput, prelude::*};
 
 // #[derive(Component, Debug)]
 // This is used to tell bevy that this is a component
@@ -27,7 +27,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         // Adding the system to the app
         .add_systems(Startup, spawn_spaceship)
-        .add_systems(Update, (update_position, print_info))
+        .add_systems(Update, (update_position, print_info, accelerate_ship))
         .run();
 }
 
@@ -35,17 +35,42 @@ fn spawn_spaceship(mut commands: Commands) {
     // We are using commands.spawn() to create a new entity
     // We are adding 2 Components to the entity Position and Velocity
     // Which makes up the Entity
-    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0 }, Rotation { angle: 0.0 }));
+    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 0.0, y: 0.0 }, Rotation { angle: 0.0 }));
+}
+
+fn accelerate_ship(KeyboardInput: Res<ButtonInput<KeyCode>>, mut query: Query<(&mut Velocity, &Rotation)>) {
+
+    let acceleration: f32 = 0.1;
+
+
+    for (mut velocity, rotation) in query.iter_mut() {
+        if KeyboardInput.pressed(KeyCode::KeyW){
+            velocity.x += acceleration * rotation.angle.cos();
+            velocity.y += acceleration * rotation.angle.sin();
+        }
+    
+        if KeyboardInput.pressed(KeyCode::KeyS) {
+           
+            let current_speed = (velocity.x.powi(2) + velocity.y.powi(2)).sqrt();
+            let new_speed = (current_speed - acceleration).max(0.0);
+
+            if current_speed > 0.0 {
+                let scale = new_speed / current_speed;
+                velocity.x *= scale;
+                velocity.y *= scale;
+            }
+         }
+    }
 }
 
 // Query is like database we are fetching the Entities that Has VELOCITY & POSITION
 // We didn't add &mut because we are not modifying the velocity we are just reading value
 // But we added &mut because we are modifying the position by using readonly velocity
-fn update_position(mut query: Query<(&Velocity, &mut Position, &Rotation)>) {
+fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
     // We are using query.iter_mut() to get the iterator of the query
-    for (velocity, mut position, rotation) in query.iter_mut() {
-        position.x += velocity.x * rotation.angle.cos();
-        position.y += velocity.y * rotation.angle.sin();
+    for (velocity, mut position) in query.iter_mut() {
+        position.x += velocity.x;
+        position.y += velocity.y;
     }
 }
 
