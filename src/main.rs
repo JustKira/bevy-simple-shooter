cf230a9a8b4a62b8586adff81c0cc502e6025fe5
justkira <shighgamerhg@gmail.com>
2024-08-19 16:42:31 +0300

    chore: #11

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use bevy::prelude::*;
 
 #[derive(Component, Debug)]
-struct Player;
+struct Player {}
+
+#[derive(Component, Debug)]
+struct SpaceCraft {
+    max_speed: f32,
+    active_speed: f32,
+    min_speed: f32,
+}
 
 #[derive(Component, Debug)]
 struct PlayerCamera;
@@ -20,7 +27,10 @@ fn main() {
         .add_plugins(DefaultPlugins)
         // Adding the system to the app
         .add_systems(Startup, step_up)
-        .add_systems(Update, (player_movement, camera_movement).chain())
+        .add_systems(
+            Update,
+            (player_movement, enemy_movement, camera_movement).chain(),
+        )
         .run();
 }
 
@@ -28,12 +38,17 @@ fn step_up(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((Camera2dBundle::default(), PlayerCamera));
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("ship_J.png"),
+            texture: asset_server.load("ship_H.png"),
             transform: Transform::from_xyz(250.0, 0.0, 0.0),
             ..default()
         },
-        Velocity { value: 200.0 },
-        Player,
+        Velocity { value: 0.0 },
+        Player {},
+        SpaceCraft {
+            max_speed: 150.0,
+            active_speed: 100.0,
+            min_speed: 30.0,
+        },
     ));
     commands.spawn((
         SpriteBundle {
@@ -41,25 +56,35 @@ fn step_up(mut commands: Commands, asset_server: Res<AssetServer>) {
             transform: Transform::from_xyz(75.0, 50.0, 0.0),
             ..default()
         },
-        Velocity { value: 150.0 },
         Enemy,
     ));
 }
 
 fn player_movement(
-    mut query: Query<(&mut Transform, &Velocity), With<Player>>,
+    mut query: Query<(&mut Transform, &mut Velocity, &SpaceCraft), With<Player>>,
     time: Res<Time>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
 ) {
-    for (mut transform, velocity2d) in query.iter_mut() {
+    for (mut transform, mut velocity, space_craft) in query.iter_mut() {
         let dir = transform.up();
 
+        let current_velocity = velocity.value;
+
+        let accel: f32 = 3.5;
+
         if keyboard_input.pressed(KeyCode::KeyW) {
-            transform.translation += dir * velocity2d.value * time.delta_seconds() * 2.15;
+            velocity.value += accel;
+            transform.translation += dir * velocity.value * time.delta_seconds();
         } else if keyboard_input.pressed(KeyCode::KeyS) {
-            transform.translation += dir * velocity2d.value * time.delta_seconds() * 0.55;
+            velocity.value -= accel;
+            if velocity.value < space_craft.min_speed {
+                velocity.value = space_craft.min_speed;
+            }
+            transform.translation += dir * velocity.value * time.delta_seconds();
         } else {
-            transform.translation += dir * velocity2d.value * time.delta_seconds();
+            let sign = space_craft.active_speed - current_velocity;
+            velocity.value += sign.signum() * accel;
+            transform.translation += dir * velocity.value * time.delta_seconds();
         }
 
         if keyboard_input.pressed(KeyCode::KeyA) {
@@ -70,13 +95,13 @@ fn player_movement(
     }
 }
 
-// fn enemy_movement(mut query: Query<(&mut Transform, &Velocity), With<Enemy>>, time: Res<Time>) {
-//     for (mut transform, velocity2d) in query.iter_mut() {
-//         let dir = transform.up();
-//         transform.translation += dir * velocity2d.value * time.delta_seconds();
-//         transform.rotate_z(1.5 * time.delta_seconds());
-//     }
-// }
+fn enemy_movement(mut query: Query<(&mut Transform, &Velocity), With<Enemy>>, time: Res<Time>) {
+    for (mut transform, velocity2d) in query.iter_mut() {
+        let dir = transform.up();
+        transform.translation += dir * velocity2d.value * time.delta_seconds();
+        transform.rotate_z(1.5 * time.delta_seconds());
+    }
+}
 
 fn camera_movement(
     mut camera: Query<&mut Transform, (With<PlayerCamera>, Without<Player>)>,
