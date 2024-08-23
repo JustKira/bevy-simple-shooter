4369174200ffb3295690c4eca4298ed99c240b12
justkira <shighgamerhg@gmail.com>
2024-08-23 22:18:47 +0300

    chore: spacecraft boilerplate

--- src/spacecraft.rs
+++ src/spacecraft.rs
@@ -1,18 +1,53 @@
+use avian2d::prelude::*;
 use bevy::prelude::*;
-
 pub struct SpaceCraftPlugin;
+
+#[derive(Component)]
+pub struct SpaceCraft {}
+
+#[derive(Component)]
+pub struct MaxSpeed {
+    pub value: f32,
+}
+
+#[derive(Component)]
+pub struct MaxRotationSpeed {
+    pub value: f32,
+}
 
 impl Plugin for SpaceCraftPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_spacecraft);
+        app.add_systems(Startup, spawn_spacecraft)
+            .add_systems(Update, (spacecraft_movement, spacecraft_rotation));
     }
 }
 
 fn spawn_spacecraft(mut commands: Commands, asset_server: Res<AssetServer>) {
-    println!("Spawning SpaceCraft");
+    commands.spawn((
+        RigidBody::Dynamic,
+        SpriteBundle {
+            texture: asset_server.load("ship_B.png"),
+            ..default()
+        },
+        GravityScale(0.0),
+        Collider::circle(10.0),
+        MaxSpeed { value: 100.0 },
+        MaxRotationSpeed { value: 5.0 },
+        SpaceCraft {},
+    ));
+}
 
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("ship_B.png"),
-        ..default()
-    });
+fn spacecraft_movement(
+    mut query: Query<(&mut LinearVelocity, &Transform, &MaxSpeed), With<SpaceCraft>>,
+    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    for (mut linear_velocity, transform, max_speed) in query.iter_mut() {}
+}
+fn spacecraft_rotation(
+    mut query: Query<(&mut AngularVelocity, &MaxRotationSpeed), With<SpaceCraft>>,
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    for (mut angular_velocity, max_rotation_speed) in query.iter_mut() {}
 }
